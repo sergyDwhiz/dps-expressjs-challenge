aa7f6c0e4ed2d78a8d29180a6a135b2bff4a43a1
Sergius Nyah <sergiusnyah@gmail.com>
2024-07-26 14:22:24 +0100

    Create project routes

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,40 @@ const authMiddleware = (req: express.Request, res: express.Response, next: expre
 	next();
 };
 
-// Define routes
+// Define routes for projects
+
+// create project
+app.post('/projects', authMiddleware, async (req, res) => {
+	const project = req.body;
+	const newProject = await dbService.createProject(project);
+	res.status(201).json(newProject);
+});
+// read (retrieve) all projects
+app.get('/projects', async (req, res) => {
+	const projects = await dbService.getProjects();
+	res.json(projects);
+});
+// read (retrieve) project by Id
+app.get('/projects/:id', async (req, res) => {
+	const id = req.params.id;
+	const project = await dbService.getProjectById(id);
+	res.json(project);
+});
+
+// update project
+app.put('/projects/:id', async (req, res) => {
+	const id = req.params.id;
+	const project = req.body;
+	const updatedProject = await dbService.updateProject(id, project);
+	res.json(updatedProject);
+});
+// delete project
+app.delete('/projects/:id', async (req, res) => {
+	const id = req.params.id;
+	await dbService.deleteProject(id);
+	res.status(204).send();
+});
+
 
 
 app.use(authMiddleware);
