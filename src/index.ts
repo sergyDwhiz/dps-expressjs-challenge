e6f3f7795b2e2d478f7dd83ca1135d44247d3a27
Sergius Nyah <sergiusnyah@gmail.com>
2024-07-26 10:13:36 +0100

    Create authentication middleware

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,23 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const app: Express = express();
+const app: Express = express(); // for parsing json data
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+// Authentication middleware
+const authToken = 'Password123';
+
+const authMiddleware = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+	const token = req.headers.authorization;
+	if (token !== authToken) {
+		return res.status(401).send('Unauthorized');
+	}
+	next();
+};
+
+app.use(authMiddleware);
 
 app.listen(port, () => {
 	console.log(`[server]: Server is running at http://localhost:${port}`);
