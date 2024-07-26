640cd271a1f7848bcee440d9d240013205a968ae
Sergius Nyah <sergiusnyah@gmail.com>
2024-07-26 14:43:24 +0100

    Create report routes

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ app.use(express.json());
 // Authentication middleware
 const authToken = process.env.AUTH_TOKEN;
 
+// Apply middleware to all routes
 const authMiddleware = (req: express.Request, res: express.Response, next: express.NextFunction) => {
 	const token = req.headers.authorization;
 	if (token !== authToken) {
@@ -20,11 +21,12 @@ const authMiddleware = (req: express.Request, res: express.Response, next: expre
 	}
 	next();
 };
+app.use(authMiddleware);
 
 // Define routes for projects
 
 // create project
-app.post('/projects', authMiddleware, async (req, res) => {
+app.post('/projects', async (req, res) => {
 	const project = req.body;
 	const newProject = await dbService.createProject(project);
 	res.status(201).json(newProject);
@@ -55,10 +57,43 @@ app.delete('/projects/:id', async (req, res) => {
 	res.status(204).send();
 });
 
+// Define routes for reports
 
-
-app.use(authMiddleware);
-
+// Create report
+app.post('/projects/:projectId/reports', async (req, res) => {
+	const report = req.body;
+	const project_id = req.params.projectId;
+    const createdReport = await dbService.createReport({...report, project_id});
+    res.status(201).json(createdReport);
+});
+// Get all reports for specific project
+app.get('/projects/:projectId/reports', async (req, res) => {
+	const projectId = req.params.projectId;
+	const reports = await dbService.getReport(projectId);
+	res.json(reports);
+});
+// Get specific report by id for a specific project
+app.get('/projects/:projectId/reports/:id', async (req, res) => {
+	const projectId = req.params.projectId;
+	const id = req.params.id;
+	const reports = await dbService.getReportById(projectId, id);
+	res.json(reports);
+});
+// update report
+app.put('/projects/:projectId/reports/:id', async (req, res) => {
+	const projectId = req.params.projectId;
+	const id = req.params.id;
+	const report = req.body;
+	const updatedReport = await dbService.updateReport(id, { ...report, project_id: projectId });
+	res.json(updatedReport);
+});
+// delete report
+app.delete('/projects/:projectId/reports/:id', async (req, res) => {
+	const projectId = req.params.projectId;
+	const id = req.params.id;
+	await dbService.deleteReport(projectId, id);
+	res.status(204).send();
+});
 app.listen(port, () => {
 	console.log(`[server]: Server is running at http://localhost:${port}`);
 });
