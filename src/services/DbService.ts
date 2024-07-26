3422d98775e3c6caa9e4c26de68230383197e7e9
Sergius Nyah <sergiusnyah@gmail.com>
2024-07-26 12:49:42 +0100

    add: return an object with information

--- src/services/DbService.ts
+++ src/services/DbService.ts
@@ -1,11 +1,11 @@
-import db from './db.service'; // for run and query commands
+import db from './db.service'; // for `run` and `query` commands
 
 export default class DbService {
     // create and return project object
     async createProject(project: {id: string, name: string, description: string }){
         const sql = 'INSERT INTO projects (id, name, description) VALUES (:id, :name, :description)';
-        db.run(sql, project);
-        return project;
+       const result = db.run(sql, project);
+        return {...project, ...result }; // return project details and result from operation
     }
     // read all projects
     async getProjects(){
@@ -20,13 +20,48 @@ export default class DbService {
     // update project by id
     async updateProject(id: string, project: {name: string, description: string}){
         const sql = 'UPDATE projects SET name = :name, description = :description WHERE id = :id';
-        db.run(sql, {...project, id});
-        return {id, ...project};
+        const result = db.run(sql, {...project, id});
+        return {id, ...project, ... result};
     }
     // delete project by id
     async deleteProject(id: string){
         const sql = 'DELETE FROM projects WHERE id = :id';
-        db.run(sql, {id});
-        return console.log(`Project with ${id} deleted`);
+        const result =  db.run(sql, {id});
+        console.log(`Project with ${id} deleted`);
+        return result;
     }
+
+    // create report
+    async createReport(report: {id: string, text: string, project_id: string}){
+        const sql = 'INSERT INTO reports (id, text, project_id) VALUES (:id, :text, :project_id)';
+        const result = db.run(sql, report);
+        return {...report, ...result};
+    }
+    //
+    // retrieve all reports associated with a specific project (could be multiple reports)
+    async getReport(project_id: string){
+        const sql = 'SELECT * FROM reports WHERE project_id = :project_id';
+        return db.query(sql, {project_id});
+    }
+
+    // retrieves a specific report by its unique ID, ensuring it belongs to the
+    // specified project. The output will be a single report object if found, or
+    // null if no matching report exists.
+    async getReportById(project_id:string, id: string){
+        const sql = 'SELECT * FROM reports WHERE project_id = :project_id AND id = :id';
+        return db.query(sql, {project_id, id});
+    }
+    // update report
+    async updateReport(id: string, report :{text: string, project_id: string}){
+        const sql = 'UPDATE reports SET text = :text, project_id = :project_id WHERE id = :id';
+        const result =  db.run(sql, {...report, id});
+        return {id, ...report, ...result};
+    }
+    // delete report
+    async deleteReport(id: string, project_id: string){
+        const sql = 'DELETE FROM reports WHERE id = :id AND project_id = :project_id';
+        const result = db.run(sql, {id, project_id});
+        return console.log(`Report with ${id} deleted`);
+    }
+
 }
